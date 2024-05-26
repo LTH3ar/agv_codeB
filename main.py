d2e8553d1e9cc48fbcae885afab2eef6820a35e5
lth3ar <letuanhuy1301@gmail.com>
2024-05-26 10:03:24 +0700

    add Forecasting module

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from agv_module import agv_solver
+from forecasting_model_module.ForecastingModel import ForecastingModel, DimacsFileReader
 
-file_reader = agv_solver.FileReader("/home/raven/git_repo/github/agv_codeB/input_dimacs/supply_03_demand_69.txt")
+file_reader = DimacsFileReader("input_dimacs/supply_03_demand_69.txt")
 file_reader.read_custom_dimacs()
 problem_info, supply_nodes_dict, demand_nodes_dict, zero_nodes_dict, arc_descriptors_dict, earliness_tardiness_dict = file_reader.get_all_dicts()
-
-model = agv_solver.MinimumCostFlowModel(supply_nodes_dict, demand_nodes_dict, zero_nodes_dict, arc_descriptors_dict, earliness_tardiness_dict)
+model = ForecastingModel(problem_info, supply_nodes_dict, demand_nodes_dict, zero_nodes_dict, arc_descriptors_dict, earliness_tardiness_dict)
 model.solve()
-solution = model.get_solution_dict()
-print(solution)
 model.output_solution()
+model.save_solution("supply_03_demand_69_edit.txt", "ForecastingModuleTestOutput")
+model.create_traces("traces.txt")
